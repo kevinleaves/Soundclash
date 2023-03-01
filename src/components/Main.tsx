0e6a14b9bee5344d483f2ef2bef3d3e71a791974
Kevin Le <iamkevinvle@gmail.com>
2023-03-01 12:18:39 -0800

    modify Main to use arrays and simplify useEffect logic

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,30 +5,20 @@ export default function Main ({ tracks }) {
   // FUTURE TODO: generate x RANDOM contestants
   // FUTURE TODO: explore alternative ways to store this data (SET, MAP, ARRAY, etc)
 
-  const competitors = {}
-  const [contestants, setContestants] = useState(competitors)
-  const [winners, setWinners] = useState({});
+  const [contestants, setContestants] = useState(tracks.slice(0, 16))
+  const [winners, setWinners] = useState([]);
   const [currentPlayers, setCurrentPlayers] = useState([])
 
-  const findSongID = (id) => {
-    const filtered = Object.entries(contestants)
-      .filter((entry) => entry[1].id === id)
-    console.log(filtered, 'filtered')
-    // returns true if song found
-    const found = filtered.length !== 0
-    if (found) {
-      // set won property to true
-      filtered[0][1].won = true
-      // delete track from competitors
-      const copy = {...contestants}
-      console.log(filtered[0][0], 'idx')
-      delete copy[filtered[0][0]]
-      // delete opposing track from competitors
-
-      // setContestants()
-    }
-    return filtered.length !== 0
-  }
+  // const findSongID = (id) => {
+  //   const filtered = contestants
+  //     .filter((entry) => entry[1].id === id)
+  //   console.log(filtered, 'filtered')
+  //   // returns true if song found
+  //   const found = filtered.length !== 0
+  //   if (found) {
+  //   }
+  //   return filtered.length !== 0
+  // }
 
   // const replacePlayers = () => {
   //   // loop through currentPlayers array. for each player: delete player from contestants
@@ -41,37 +31,25 @@ export default function Main ({ tracks }) {
 
   const handleClick = (event, songID) => {
     console.log(songID, 'songid')
-    findSongID(songID)
+    // findSongID(songID)
   }
 
   useEffect(() => {
-    const mapped = tracks.map((track) => {
-      return {...track, won: null}
-    })
+    console.log(contestants)
+      // // generate 2 random indices to start 0-15
+    let randomIndex1 = Math.floor(Math.random() * contestants.length);
+    let randomIndex2 = Math.floor(Math.random() * contestants.length);
 
-    const generateContestants = () => {
-      for (let i = 0; i < 16; i++) {
-        competitors[i] = mapped[i]
-      }
-      const keys = Object.keys(competitors)
-
-      // generate 2 random indices to start 0-15
-      let randomIndex1 = Math.floor(Math.random() * keys.length);
-      let randomIndex2 = Math.floor(Math.random() * keys.length);
-
-      // while the 2 indices aren't equal
-      while (randomIndex1 === randomIndex2) {
-        randomIndex2 = Math.floor(Math.random() * keys.length);
-      }
-      // assign player to index
-      const player1 = competitors[randomIndex1]
-      const player2 = competitors[randomIndex2]
+    // while the 2 indices aren't equal
+    while (randomIndex1 === randomIndex2) {
+      randomIndex2 = Math.floor(Math.random() * contestants.length);
+    }
+    // assign player to index
+      const player1 = contestants[randomIndex1]
+      const player2 = contestants[randomIndex2]
 
       setCurrentPlayers([player1, player2])
-
-    }
-    generateContestants()
-    console.log(contestants, 'contestants')
+      console.log(currentPlayers, 'players')
   }, [])
 
   return (
