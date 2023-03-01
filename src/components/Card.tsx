b8469d88ecc594bfa5a3b81275c0091959083730
Kevin Le <iamkevinvle@gmail.com>
2023-03-01 08:04:48 -0800

    add initial contestants to tournament

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,16 +8,30 @@ export default function Card ({ track }) {
         return acc + (index === 0 ? '' : ', ') + artist.name;}, '')
     return artist
   }
+
+  const condensedTrack = {
+    id: track.id,
+    name: track.name,
+    artist: generateArtistString(track),
+    albumName: track.album.name,
+    art: track.album.images[0].url,
+    previewURL: track.preview_url,
+    won: null
+  }
+
   return (
     <div className='flex flex-col'>
       CARD
       <img
-        src={track.album.images[0].url}
+        src={condensedTrack.art}
         />
-      <p className='font-semibold text-xl'>{track.name}</p>
-      <p className='font-extralight text-xl'>{generateArtistString(track)}</p>
-      <p className='font-extralight text-xl'>{track.album.name}</p>
-      <a href={track.preview_url}>SNIPPET </a>
+      <p className='font-semibold text-xl'>{condensedTrack.name}</p>
+      <p className='font-extralight text-xl'>{condensedTrack.artist}</p>
+      <p className='font-extralight text-xl'>{condensedTrack.albumName}</p>
+      <a href={condensedTrack.previewURL}>SNIPPET</a>
+      <button onClick={() => console.log(condensedTrack.id)}>
+        CHOOSE
+      </button>
     </div>
   )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from './Card'
 
 export default function Main ({ tracks }) {
+  // FUTURE TODO: generate x RANDOM contestants
+  const competitors = {}
+  const [contestants, setContestants] = useState(competitors)
+  const [winners, setWinners] = useState({});
 
-  console.log(tracks, 'inmai')
+  useEffect(() => {
+    const mapped = tracks.map((track) => {
+      return {...track, won: null}
+    })
+    const generateContestants = () => {
+      for (let i = 0; i < 15; i++) {
+        competitors[i] = mapped[i]
+      }
+    }
+    generateContestants()
+  }, [])
 
   return (
     <>
-      Main
       <div className='flex gap-8 mb-80'>
         <Card track={tracks[0]}/>
         <Card track={tracks[1]}/>
